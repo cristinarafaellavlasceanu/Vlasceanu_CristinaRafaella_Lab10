f284ab393dcc10b74f9e98a1ebb9009f49013385
Cristina Rafaella Vlasceanu <rafa.vlasceanu@yahoo.com>
2022-01-08 11:04:26 +0200

    Add project files.

---
+++ Vlasceanu_CristinaRafaella_Lab10/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Vlasceanu_CristinaRafaella_Lab10.Data;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Vlasceanu_CristinaRafaella_Lab10
+{
+    public partial class App : Application
+    {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new ListEntryPage());
+
+            //MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
